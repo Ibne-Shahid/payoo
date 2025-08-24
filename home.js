3c74748d7902027df2c4d211d2d03a2fdbe8c1aa
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-08-25 02:55:42 +0600

    Cashout page done

--- home.js
+++ home.js
@@ -1,12 +1,15 @@
+const validPin = 1234
+
+// Add money option
+
 document.getElementById("add-money-btn").addEventListener('click', function (e) {
     e.preventDefault()
-
-    const pin = 4321
 
     const bank = document.getElementById("bank").value
     const addedAmount = parseInt(document.getElementById("added-amount").value)
     const accoutnNumber = document.getElementById("account-number").value
-    const pinNumber = parseInt(document.getElementById("pin-number").value)
+    const pinNumber = parseInt(document.getElementById("add-pin-number").value)
+    const balance = parseInt(document.getElementById("balance").innerText)
 
     if (bank === "Select Bank") {
         alert("Please select a bank!")
@@ -18,21 +21,56 @@ document.getElementById("add-money-btn").addEventListener('click', function (e) 
         return;
     }
 
-    if (pinNumber !== pin) {
+    if (pinNumber !== validPin) {
         alert("Your pin is incorrect!")
         return;
     }
 
-    if (addedAmount <= 0) {
+    if (isNaN(addedAmount) || addedAmount <= 0) {
         alert("Please enter a valid amount!");
         return;
     }
 
-    const balance = parseInt(document.getElementById("balance").innerText)
-
     const newBalance = addedAmount + balance
 
     document.getElementById("balance").innerText = newBalance   
+
+
+})
+
+// Cash out option
+
+document.getElementById("cash-out-btn").addEventListener('click', function(e){
+    e.preventDefault()
+
+    const withdrawnAmount = parseInt(document.getElementById("withdrawn-amount").value)
+    const agentNumber = document.getElementById("agent-number").value
+    const pinNumber = parseInt(document.getElementById("cash-pin-number").value)
+    const balance = parseInt(document.getElementById("balance").innerText)
+
+    if(agentNumber.length != 11){
+        alert("Please provide a valid number!")
+        return;
+    }
+
+    if(pinNumber !== validPin){
+        alert("Your pin is incorrect!")
+        return;
+    }
+
+    if(isNaN(withdrawnAmount) || withdrawnAmount <= 0){
+         alert("Please enter a valid amount!");
+        return;
+    }
+
+    if(withdrawnAmount>balance){
+        alert("Insufficient balance!")
+        return;
+    }
+
+    const availableBalance = balance - withdrawnAmount
+
+    document.getElementById("balance").innerText = availableBalance
 
 
 })
