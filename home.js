ed4f7800a992653e16475c9523d0bde6696be4a2
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-08-26 02:56:17 +0600

    Project done

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
-const validPin = 1234
+const validPin = 1234;
+const transactionData = [];
 
 // Add money option
 
@@ -16,7 +17,45 @@ document.getElementById("add-money-btn").addEventListener('click', function (e) 
         return;
     }
 
-    if (accoutnNumber.length != 11) {
+    if (isNaN(accoutnNumber) || accoutnNumber.length != 11) {
+        alert("Please provide a valid number!")
+        return;
+    }
+
+    if (isNaN(addedAmount) || addedAmount <= 0) {
+        alert("Please enter a valid amount!");
+        return;
+    }
+
+    if (pinNumber !== validPin) {
+        alert("Your pin is incorrect!")
+        return;
+    }
+
+    const newBalance = addedAmount + balance
+
+    document.getElementById("balance").innerText = newBalance
+
+    const data = {
+        name: "Add Money",
+        date: new Date().toLocaleTimeString()
+    }
+
+    transactionData.push(data)
+
+})
+
+// Cash out option
+
+document.getElementById("cash-out-btn").addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const withdrawnAmount = parseInt(document.getElementById("withdrawn-amount").value)
+    const agentNumber = document.getElementById("agent-number").value
+    const pinNumber = parseInt(document.getElementById("cash-pin-number").value)
+    const balance = parseInt(document.getElementById("balance").innerText)
+
+    if (isNaN(accoutnNumber) || agentNumber.length != 11) {
         alert("Please provide a valid number!")
         return;
     }
@@ -26,44 +65,12 @@ document.getElementById("add-money-btn").addEventListener('click', function (e) 
         return;
     }
 
-    if (isNaN(addedAmount) || addedAmount <= 0) {
+    if (isNaN(withdrawnAmount) || withdrawnAmount <= 0) {
         alert("Please enter a valid amount!");
         return;
     }
 
-    const newBalance = addedAmount + balance
-
-    document.getElementById("balance").innerText = newBalance   
-
-
-})
-
-// Cash out option
-
-document.getElementById("cash-out-btn").addEventListener('click', function(e){
-    e.preventDefault()
-
-    const withdrawnAmount = parseInt(document.getElementById("withdrawn-amount").value)
-    const agentNumber = document.getElementById("agent-number").value
-    const pinNumber = parseInt(document.getElementById("cash-pin-number").value)
-    const balance = parseInt(document.getElementById("balance").innerText)
-
-    if(agentNumber.length != 11){
-        alert("Please provide a valid number!")
-        return;
-    }
-
-    if(pinNumber !== validPin){
-        alert("Your pin is incorrect!")
-        return;
-    }
-
-    if(isNaN(withdrawnAmount) || withdrawnAmount <= 0){
-        alert("Please enter a valid amount!");
-        return;
-    }
-
-    if(withdrawnAmount>balance){
+    if (withdrawnAmount > balance) {
         alert("Insufficient balance!")
         return;
     }
@@ -72,12 +79,18 @@ document.getElementById("cash-out-btn").addEventListener('click', function(e){
 
     document.getElementById("balance").innerText = availableBalance
 
+    const data = {
+        name: "Cash Out",
+        date: new Date().toLocaleTimeString()
+    }
+
+    transactionData.push(data)
 
 })
 
 // Transfer money page
 
-document.getElementById("transfer-btn").addEventListener('click', function(e){
+document.getElementById("transfer-btn").addEventListener('click', function (e) {
     e.preventDefault
 
     const transferAmount = parseInt(document.getElementById("transfer-amount").value)
@@ -85,22 +98,22 @@ document.getElementById("transfer-btn").addEventListener('click', function(e){
     const pinNumber = parseInt(document.getElementById("transfer-pin-number").value)
     const balance = parseInt(document.getElementById("balance").innerText)
 
-    if(userNumber.length != 11){
+    if (isNaN(accoutnNumber) || userNumber.length != 11) {
         alert("Please provide a valid number!")
         return;
     }
 
-    if(pinNumber!==validPin){
+    if (pinNumber !== validPin) {
         alert("Your pin is incorrect!")
         return;
     }
 
-    if(isNaN(transferAmount) || transferAmount <= 0){
+    if (isNaN(transferAmount) || transferAmount <= 0) {
         alert("Please enter a valid amount!");
         return;
     }
 
-    if(transferAmount > balance){
+    if (transferAmount > balance) {
         alert("Insufficient balance!")
         return;
     }
@@ -108,11 +121,19 @@ document.getElementById("transfer-btn").addEventListener('click', function(e){
     const availableBalance = balance - transferAmount
 
     document.getElementById("balance").innerText = availableBalance
+
+    const data = {
+        name: "Transfer Money",
+        date: new Date().toLocaleTimeString()
+    }
+
+    transactionData.push(data)
+
 })
 
 // Get bonus page
 
-document.getElementById("bonus-btn").addEventListener('click', function(e){
+document.getElementById("bonus-btn").addEventListener('click', function (e) {
     e.preventDefault()
 
     const luckyNumbers = ['N3LKY10K', 'N2LKY20K', 'N1LKY30K']
@@ -121,24 +142,30 @@ document.getElementById("bonus-btn").addEventListener('click', function(e){
     const balance = parseInt(document.getElementById("balance").innerText)
     let newBalance = balance;
 
-    if(couponNumber===luckyNumbers[0]){
+    if (couponNumber === luckyNumbers[0]) {
         newBalance = balance + 10000
-    } else if (couponNumber===luckyNumbers[1]){
+    } else if (couponNumber === luckyNumbers[1]) {
         newBalance = balance + 20000
-    } else if (couponNumber===luckyNumbers[2]){
+    } else if (couponNumber === luckyNumbers[2]) {
         newBalance = balance + 30000
     } else {
         alert("Your coupon is invalid!")
     }
 
     document.getElementById("balance").innerText = newBalance
-    
-    
+
+    const data = {
+        name: "Get Bonus",
+        date: new Date().toLocaleTimeString()
+    }
+
+    transactionData.push(data)
+
 })
 
 // Pay bill page
 
-document.getElementById("pay-now-btn").addEventListener('click', function(e){
+document.getElementById("pay-now-btn").addEventListener('click', function (e) {
     e.preventDefault()
 
     const bank = document.getElementById("bill-bank").value
@@ -147,27 +174,27 @@ document.getElementById("pay-now-btn").addEventListener('click', function(e){
     const pinNumber = parseInt(document.getElementById("paying-pin-number").value)
     const balance = parseInt(document.getElementById("balance").innerText)
 
-    if(bank === "Select Bank"){
+    if (bank === "Select Bank") {
         alert("Please select a bank!")
         return;
     }
 
-    if(accoutnNumber.length != 8){
+    if (isNaN(accoutnNumber) || accoutnNumber.length != 8) {
         alert("Please provide a valid account number!")
         return;
     }
 
-    if(isNaN(payingAmount) || payingAmount <= 0){
+    if (isNaN(payingAmount) || payingAmount <= 0) {
         alert("Please enter a valid amount!");
         return;
     }
 
-    if(pinNumber !== validPin){
+    if (pinNumber !== validPin) {
         alert("Your pin is incorrect!")
         return;
     }
 
-    if(payingAmount > balance){
+    if (payingAmount > balance) {
         alert("Insufficient balance!")
         return;
     }
@@ -175,65 +202,108 @@ document.getElementById("pay-now-btn").addEventListener('click', function(e){
     const availableBalance = balance - payingAmount
 
     document.getElementById("balance").innerText = availableBalance
+
+    const data = {
+        name: "Pay Bill",
+        date: new Date().toLocaleTimeString()
+    }
+
+    transactionData.push(data)
+
+})
+
+// Transaction page
+
+document.getElementById("transaction-option").addEventListener('click', function () {
+    const transactionContainer = document.getElementById("transaction-container")
+    transactionContainer.innerHTML = ""
+
+    for (const data of transactionData) {
+        const div = document.createElement('div')
+
+        div.innerHTML = `
+        <div class=" bg-white rounded-xl p-3 flex justify-between items-center mt-3">
+                    <div class="flex items-center">
+                        <div class="p-3 rounded-full bg-[#f4f5f7]">
+                            <img src="./assets/wallet1.png" class="mx-auto" alt="" />
+                        </div>
+                        <div class="ml-4">
+                            <h1 class="font-semibold">${data.name}</h1>
+                            <p>${data.date}</p>
+                        </div>
+                    </div>
+
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+        `
+
+        transactionContainer.appendChild(div)
+
+    }
 })
 
 // Function for toggle
 
-function handleToggle (id){
+function handleToggle(id) {
     const forms = document.getElementsByClassName("form")
 
-    for (const form of forms){
-        form.style.display= "none"
+    for (const form of forms) {
+        form.style.display = "none"
     }
 
-    document.getElementById(id).style.display= "block"
+    document.getElementById(id).style.display = "block"
 }
 
-function handleBg (id){
+function handleBg(id) {
     const formbtns = document.getElementsByClassName("toggle-bg")
-    for(const btn of formbtns){
-        btn.classList.remove("border-[#0874f2]","bg-[#0874f20d]")
+    for (const btn of formbtns) {
+        btn.classList.remove("border-[#0874f2]", "bg-[#0874f20d]")
         btn.classList.add("border-gray-300")
     }
     document.getElementById(id).classList.remove("border-gray-300")
-    document.getElementById(id).classList.add("border-[#0874f2]","bg-[#0874f20d]")
-    
+    document.getElementById(id).classList.add("border-[#0874f2]", "bg-[#0874f20d]")
+
 }
 
 // Toggle Feature
 
-document.getElementById("add-money-option").addEventListener('click', function(){
+document.getElementById("add-money-option").addEventListener('click', function () {
     handleToggle("add-money-page")
     handleBg("add-money-option")
 })
 
-document.getElementById("cash-out-option").addEventListener('click', function(){
+document.getElementById("cash-out-option").addEventListener('click', function () {
     handleToggle("cash-out-page")
     handleBg("cash-out-option")
 })
 
-document.getElementById("transfer-money-option").addEventListener('click', function(){
+document.getElementById("transfer-money-option").addEventListener('click', function () {
     handleToggle("transfer-money-page")
     handleBg("transfer-money-option")
 })
 
-document.getElementById("get-bonus-option").addEventListener('click', function(){
+document.getElementById("get-bonus-option").addEventListener('click', function () {
     handleToggle("get-bonus-page")
     handleBg("get-bonus-option")
 })
 
-document.getElementById("pay-bill-option").addEventListener('click', function(){
+document.getElementById("pay-bill-option").addEventListener('click', function () {
     handleToggle("pay-bill-page")
     handleBg("pay-bill-option")
 })
 
+document.getElementById("transaction-option").addEventListener('click', function(){
+    handleToggle("transaction-page")
+    handleBg("transaction-option")
+})
+
 // Logout Button
 
-document.getElementById("logout-btn").addEventListener('click', function(){
+document.getElementById("logout-btn").addEventListener('click', function () {
     let userConfirmed = confirm("Are you sure?")
 
-    if(userConfirmed){
-        window.location.href= "./index.html"
+    if (userConfirmed) {
+        window.location.href = "./index.html"
     } else {
         alert("You clicked NO, stay logged in.")
     }
